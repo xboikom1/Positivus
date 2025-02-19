18012e3d2c80f3aa3dacb75bbcdfb2c0dc7361a3
Maxim_Boiko <Maksimboiko222@gmail.com>
2025-02-19 23:25:34 +0100

    hero section update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
+import css from './Header.module.css';
+
 const Header = () => {
   return (
-    <>
-      <div>
-        <h1>Navigating the digital landscape for success</h1>
-        <p>
+    <div className={css.headerContainer}>
+      <div className={css.textContainer}>
+        <h1 className={css.title}>
+          Navigating the digital landscape for success
+        </h1>
+        <p className={css.text}>
           Our digital marketing agency helps businesses grow and succeed online
           through a range of services including SEO, PPC, social media
           marketing, and content creation.
         </p>
-        <button type="button">Book a consultation</button>
+        <button type="button" className={css.bookBtn}>
+          Book a consultation
+        </button>
       </div>
-    </>
+      <img src="/src/images/Illustration.png" alt="" />
+    </div>
   );
 };
 
--- src/components/Logotypes/Logotypes.jsx
+++ src/components/Logotypes/Logotypes.jsx
@@ -1,8 +1,81 @@
+import css from './Logotypes.module.css';
+
 const Logotypes = () => {
   return (
-    <>
-      
-    </>
+    <ul className={css.linkList}>
+      <li className={css.linkListItem}>
+        <a className={css.link} href="https://www.amazon.com" target="blank">
+          <svg
+            className="logo-icon"
+            width="124px"
+            height="48px"
+            aria-label="Amazon logo"
+          >
+            <use href="/src/images/icons/sprite.svg#icon-amazon"></use>
+          </svg>
+        </a>
+      </li>
+      <li className={css.linkListItem}>
+        <a className={css.link} href="https://dribbble.com" target="blank">
+          <svg
+            className="logo-icon"
+            width="128px"
+            height="48px"
+            aria-label="Dribble logo"
+          >
+            <use href="/src/images/icons/sprite.svg#icon-dribble"></use>
+          </svg>
+        </a>
+      </li>
+      <li className={css.linkListItem}>
+        <a className={css.link} href="https://www.hubspot.com" target="blank">
+          <svg
+            className="logo-icon"
+            width="124px"
+            height="48px"
+            aria-label="Hubspot logo"
+          >
+            <use href="/src/images/icons/sprite.svg#icon-hubspot"></use>
+          </svg>
+        </a>
+      </li>
+      <li className={css.linkListItem}>
+        <a className={css.link} href="./index.html" target="blank">
+          <svg
+            className="logo-icon"
+            width="124px"
+            height="48px"
+            aria-label="Positivus logo"
+          >
+            <use href="/src/images/icons/sprite.svg#icon-amazon"></use>
+          </svg>
+        </a>
+      </li>
+      <li className={css.linkListItem}>
+        <a className={css.link} href="./index.html" target="blank">
+          <svg
+            className="logo-icon"
+            width="124px"
+            height="48px"
+            aria-label="Positivus logo"
+          >
+            <use href="/src/images/icons/sprite.svg#icon-amazon"></use>
+          </svg>
+        </a>
+      </li>
+      <li className={css.linkListItem}>
+        <a className={css.link} href="./index.html" target="blank">
+          <svg
+            className="logo-icon"
+            width="124px"
+            height="48px"
+            aria-label="Positivus logo"
+          >
+            <use href="/src/images/icons/sprite.svg#icon-amazon"></use>
+          </svg>
+        </a>
+      </li>
+    </ul>
   );
 };
 
