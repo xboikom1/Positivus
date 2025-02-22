026fb71d064bc838a01e7574a0f91b036fd58889
Maxim_Boiko <Maksimboiko222@gmail.com>
2025-02-22 16:48:14 +0100

    CTA block

--- src/components/CTA Block/CTA block.jsx
+++ src/components/CTA Block/CTA block.jsx
@@ -1,9 +1,26 @@
+import clsx from 'clsx';
 import css from './CTA block.module.css';
 
 const CTA_block = () => {
   return (
     <>
-      <section className="section"></section>
+      <section className={clsx(css.sectionContainer, 'section')}>
+        <div className={css.textContainer}>
+          <h3 className={css.title}>Let’s make things happen</h3>
+          <p>
+            Contact us today to learn more about how our digital marketing
+            services can help your business grow and succeed online.
+          </p>
+          <button type="button" className={css.btn}>
+            Get your free proposal
+          </button>
+        </div>
+        <img
+          src="/src/images/CTA-image.png"
+          alt="Decorative image"
+          className={css.image}
+        />
+      </section>
     </>
   );
 };
