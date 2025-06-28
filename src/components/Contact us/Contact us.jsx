d20e87dcc8044bfda6e1b27811765c644d547cbc
Maxim_Boiko <Maksimboiko222@gmail.com>
2025-06-29 00:27:21 +0200

    responsive contact form

--- src/components/Contact us/Contact us.jsx
+++ src/components/Contact us/Contact us.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
       <div className="titleContainer">
         <h2 className={css.title}>Contact Us</h2>
         <p className={css.titleText}>
-          Connect with Us: Let's Discuss Your Digital Marketing Needs
+          Connect with Us: Let&apos;s Discuss Your Digital Marketing Needs
         </p>
       </div>
       <Contact_Form />
